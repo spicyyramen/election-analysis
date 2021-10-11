494faeae0d1393ac78a6f794cda7d1f27a52086d
spicyyramen <88313330+spicyyramen@users.noreply.github.com>
2021-10-11 03:15:44 -0700

    updated readme

--- PyPoll_Challenge_starter_code.py
+++ PyPoll_Challenge_starter_code.py
@@ -6,7 +6,7 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
@@ -30,7 +30,7 @@ winning_percentage = 0
 # 2: Track the largest county and county voter turnout.
 lg_county_name = ""
 lg_county_votes = 0
-
+lg_county_percent = 0
 
 # Read the csv and convert it into a list of dictionaries
 with open(file_to_load) as election_data:
@@ -95,25 +95,41 @@ with open(file_to_save, "w") as txt_file:
     # 6a: Write a for loop to get the county from the county dictionary.
     for county_name in county_votes:
         # 6b: Retrieve the county vote count.
-        county_total = county_votes
+        county_total = county_votes[county_name]
         # 6c: Calculate the percentage of votes for the county.
-        county_percent = float(county_votes)/float(county_total)*100
+        county_percent = float(county_total)/float(total_votes)*100
 
          # 6d: Print the county results to the terminal.
-        print(f"{county_name} has {county_percent} of the total votes, with {county_votes} total votes")
+        print(f"{county_name}: {county_percent:.1f}% ({county_votes[county_name]:,})")
          # 6e: Save the county votes to a text file.
-        
+         ## create variable
+        county_results = (
+            f"{county_name}: {county_percent:.1f}% ({county_votes[county_name]:,})\n"
+
+        )
+
+        txt_file.write(county_results)
+
          # 6f: Write an if statement to determine the winning county and get its vote count.
-        if (county_votes > lg_county_votes) and (county_percent > lg_county_percent):
+        if (county_total > lg_county_votes) and (county_percent > lg_county_percent):
             lg_count = lg_county_votes 
             lg_county_name = county_name
             lg_county_percent = county_percent
 
     # 7: Print the county with the largest turnout to the terminal.
-    print(f"\n lg_county_name")
+    print(
+        f"\n--------------------------------"
+        f"\n Largest County Turnout: {lg_county_name}"
+        f"\n---------------------------------"
+        )
 
     # 8: Save the county with the largest turnout to a text file.
+    txt_file.write(
+        f"\n--------------------------------"
+        f"\n Largest County Turnout: {lg_county_name}"
+        f"\n---------------------------------\n"
 
+    )
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
--- README.md
+++ README.md
@@ -19,4 +19,16 @@ The analysis of the election shows that:
 	- Charles Casper Stockham: **23.0%** of the total vote and **85,213** total votes
 	- Diana DeGette: **73.8%** of the total vote and **272,892** total votes
 	- Raymon Anthony Doane: **3.1%** of the total vote and **11,606** total votes
+- The winning candidate was:
+	- Diana Degette: 73.8% (272,892)
+- The votes broken down by county were as follows:
+	- Jefferson: **10.5%** of votes, **38,855** total votes
+	- Denver: **82.8%** of votes, **306,055** total votes
+	- Arapahoe: **6.7%** of votes, **24,801** total votes
+- The county with the largest number of votes:
+	- Denver: 8.28% (306,055)
 
+## Election-Audit Summary
+With just a few modifications, this script can be used to analyze essentially any election. If you needed to import a different csv file with election data, you would just need to verify that the column indices (ballot ID, county, candidate) were in the same order as the election_results file. If they match, the only thing you should need to change are the names of the csv file being imported, and the text file being output. 
+
+If the column indices don't match, you can just change the index numbers used in the `candidate_name = row[2]` and `county_name = row[1]`. 
